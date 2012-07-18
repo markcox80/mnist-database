945bae2810dadb9a4999f28a292f0b2e4b10ab1d
Mark Cox <markcox80@gmail.com>
2012-07-18 18:45:15 +1000

    Added documentation strings to src/image-file.lisp.

--- src/image-file.lisp
+++ src/image-file.lisp
@@ -7,36 +7,54 @@
   (:documentation "The number of columns in each image contained in DATA."))
 
 (defgeneric number-of-images (data)
-  (:documentation "Return the number of images contained in DATA."))
+  (:documentation "The number of images contained in DATA."))
 
 (defgeneric image (data index)
-  (:documentation "Return the grayscale image in DATA at position INDEX."))
+  (:documentation "Obtain the grayscale image in DATA at position INDEX.
+
+The integer value INDEX should satisfy 0 <= INDEX < (NUMBER-OF-IMAGES DATA)."))
 
 (defgeneric map-images (function data)
-  (:documentation "Call FUNCTION for each image in DATA."))
+  (:documentation "Call FUNCTION for each image in DATA.
+
+FUNCTION must accept one argument, the read image. Each time FUNCTION
+is called, a new image is created. The argument passed to function is
+of type (simple-array (unsigned-byte 8) (* *)).
+
+MAP-IMAGES is undefined if IMAGE is called inside FUNCTION."))
 
 (defclass image-data ()
   ((number-of-items
     :initarg :number-of-items
-    :reader number-of-items)
+    :reader number-of-items
+    :documentation "The number of items stored in the image IDX file
+    according to the IDX file header.")
    (rows
     :initarg :rows
-    :reader number-of-rows-per-image)
+    :reader number-of-rows-per-image
+    :documentation "The number of rows for each image in the IDX
+    file.")
    (columns
     :initarg :columns
-    :reader number-of-columns-per-image)
+    :reader number-of-columns-per-image
+    :documentation "The number of columns for each image in the IDX
+    file.")
    (stream
     :initarg :stream
-    :reader data-stream)
+    :reader data-stream
+    :documentation "The stream used to read data from the IDX file.")
    (pathname
     :initarg :pathname
-    :reader data-pathname)))
+    :reader data-pathname
+    :documentation "The pathname to the opened IDX file.")))
 
 (defmethod number-of-images ((data image-data))
   (number-of-items data))
 
-(defun open-image-data (pathname &key (if-does-not-exist :error))
-  (let ((stream (open pathname :element-type '(unsigned-byte 8) :if-does-not-exist if-does-not-exist)))
+(defun open-image-data (pathname)
+  "Open the image IDX file at PATHNAME for reading. The object
+returned satisfies the image IDX object protocol."
+  (let ((stream (open pathname :element-type '(unsigned-byte 8))))
     (unless (= 2051 (read-32-bit-integer stream))
       (error "Magic number 2051 has not been found."))
     (make-instance 'image-data
@@ -88,15 +106,21 @@
   nil)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (defun do-with-image-data (function pathname &key (if-does-not-exist :error))
-    (with-open-data (d (open-image-data pathname :if-does-not-exist if-does-not-exist))
+  (defun do-with-image-data (function pathname)
+    "Call FUNCTION with a data object obtained from (OPEN-IMAGE-DATA
+PATHNAME). When control leaves FUNCTION, either normally or
+abnormally, the data object is automatically closed using CLOSE-DATA."
+    (with-open-data (d (open-image-data pathname))
       (funcall function d)))
 
-  (defmacro with-image-data ((var pathname &key (if-does-not-exist :error)) &body body)
+  (defmacro with-image-data ((var pathname) &body body)
+    "A more convenient interface to DO-WITH-IMAGE-DATA."
     `(do-with-image-data #'(lambda (,var)
 			     ,@body)
-       ,pathname :if-does-not-exist ,if-does-not-exist)))
+       ,pathname)))
 
 (defmethod map-images (function (pathname string))
+  "Open the image IDX file at PATHNAME and call MAP-IMAGES on the data
+object."
   (with-image-data (data pathname)
     (map-images function data)))
