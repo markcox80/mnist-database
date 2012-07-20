80ed5965fed98a8d19bbadd499d7a0394cafff77
Mark Cox <markcox80@gmail.com>
2012-07-20 19:09:06 +1000

    Cleaned up more documentation strings in src/experiments.lisp.

--- src/experiments.lisp
+++ src/experiments.lisp
@@ -10,10 +10,11 @@ PATH-TO-MNIST-DATABASE.")
   "Obtain the directory pathname to the files found on the MNIST
 database website (http://yann.lecun.com/exdb/mnist/).
 
-The path to the database is obtained by first checking if a pathname
-has been assigned to *PATH-TO-MNIST-DATABASE*. If not, then a check to
-see if a logical pathname translation exists for the logical host
-MNIST-DATABASE. Otherwise an error is signalled."
+The path returned is determined in the following order (i) the value
+of *PATH-TO-MNIST-DATABASE* (ii) a logical pathname using the host
+MNIST-DATABASE.
+
+An error is signalled if no path can be determined."
   (or *path-to-mnist-database*
       (handler-case (make-pathname :host "MNIST-DATABASE"
 				   :directory '(:absolute))
@@ -22,26 +23,24 @@ MNIST-DATABASE. Otherwise an error is signalled."
       (error "MNIST-DATABASE:*PATH-TO-MNIST-DATABASE* and is unset. MNIST-DATABASE logical host can also be used.")))
 
 (defun training-image-data-pathname ()
-  "Obtain the pathname of the image data that is to be used during
+  "Return the pathname of the image data that is used during
 training."
   (make-pathname :name "train-images-idx3-ubyte"
 		 :defaults (path-to-mnist-database)))
 
 (defun training-label-data-pathname ()
-  "Obtain the pathname of the label data that is to be used during
+  "Return the pathname of the label data that is used during
 training."
   (make-pathname :name "train-labels-idx1-ubyte"
 		 :defaults (path-to-mnist-database)))
 
 (defun testing-image-data-pathname ()
-  "Obtain the pathname of the image data that is to be used during
-testing."
+  "Return the pathname of the image data to be used during testing."
   (make-pathname :name "t10k-images-idx3-ubyte"
 		 :defaults (path-to-mnist-database)))
 
 (defun testing-label-data-pathname ()
-  "Obtain the pathname of the label data that is to be used during
-testing."
+  "Return the pathname of the label data to be used during testing."
   (make-pathname :name "t10k-labels-idx1-ubyte"
 		 :defaults (path-to-mnist-database)))
 
