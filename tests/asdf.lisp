151ac74b1e9f8299bb0225ffd4d8d28a7e3abc34
Mark Cox <markcox80@gmail.com>
2013-06-24 16:09:59 +1000

    Upgraded tests/asdf.lisp to using the new lisp-unit interface.

--- tests/asdf.lisp
+++ tests/asdf.lisp
@@ -27,4 +27,4 @@
 (in-package "CL-USER")
 
 (defmethod asdf:perform ((operation asdf:test-op) (component (eql (asdf:find-system "mnist-database-tests"))))
-  (lisp-unit:run-all-tests "MNIST-DATABASE.TESTS"))
+  (lisp-unit:run-tests :all "MNIST-DATABASE.TESTS"))
